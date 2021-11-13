ca3b63b07071821a67681ee5a2c1b062a66c0edb
Jason <eric870615@yahoo.com.tw>
2021-11-13 21:17:04 +0800

    fix queens function

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ queenArray3 = [[1,0],[0,1]]; #(1,0)
 
 # print('123');
 
-def queen(list):
-    print(f'queen function {list}');
+# def queen(list):
+#     print(f'queen function {list}');
 
 # queen(queenArray);
 
@@ -45,10 +45,37 @@ def failCheck(quennNowArray, nextQueen):
             return True
     return False
 
-def queens(quennNowArray, chessSize=2):
+
+# if not conflict(state, pos):
+#     ans += [(pos,)+ result for result in queens(n, state + (pos,))]
+
+def queens(quennNowArray=(), chessSize=2):
+    if len(quennNowArray) == chessSize: 
+        return [()]
     queenArray =[]
     for i in range(chessSize):
         if not failCheck(quennNowArray, i):
+            print([(i,)  +i ]) # result for result in queens(quennNowArray + (i,), chessSize)])
             queenArray=0
 print(failCheck((3,),1));
 
+queens();
+
+
+
+# it's great function !! 
+
+# def conflict(state, nextX):    
+#     nextY = len(state)
+#     return any(abs(state[i] - nextX) in (0, nextY - i) for i in range(nextY))
+
+# def queens2(n, state=()):
+#     if len(state) == n: 
+#         return [()]
+#     ans = []
+#     for pos in range(n):
+#         if not conflict(state, pos):
+#             ans += [(pos,)+ result for result in queens2(n, state + (pos,))]
+#     return ans
+
+# print(queens2(7))
